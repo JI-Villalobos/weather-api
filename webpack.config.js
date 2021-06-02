64927382913a95f854eb5897b4b2fcad1eedac8e
Chuy <chuy12villalobos@gmail.com>
2021-06-01 19:48:22 -0500

    [FIX] dev server

--- webpack.config.js
+++ webpack.config.js
@@ -89,10 +89,4 @@ module.exports = {
         new Dotenv(),
         new CleanWebpackPlugin(),
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        historyApiFallback: true,
-        port: 3006,
-    },
 }
