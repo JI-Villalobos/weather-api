a7d387652566e00a7f8cc6dc705d2d5ba5935330
Chuy <chuy12villalobos@gmail.com>
2021-06-08 19:25:35 -0500

    [ADD] create env variables

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
 fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`)
+fs.writeFileSync('./.env', `ICON_URL=${process.env.ICON_URL}\n`)
+fs.writeFileSync('./.env', `URL=${process.env.URL}\n`)
